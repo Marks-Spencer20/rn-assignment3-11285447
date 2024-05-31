11eb6d8627c355bb0250a008d626d1e64aeb4092
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-05-31 13:55:43 +0000

    I'm back

--- MyApp/App.js
+++ MyApp/App.js
@@ -2,17 +2,17 @@ import React, { useState } from 'react';
 import { View, Text, ScrollView, TextInput, StyleSheet, Button, FlatList, Image } from 'react-native';
 import CategoryItem from './components/CategoryItem';
 import TaskItem from './components/TaskItem';
-import profile from './profile.png'
+
 
 const categories = [
-  { name: 'Exercise', icon: './pic1.png' },
-  { name: 'Study', icon: './exercise.png' },
-  { name: 'Code', icon: './code.png' },
-  { name: 'Cook', icon: './cook.png' },
-  { name: 'Read', icon: './read.png' },
-  { name: 'Travel', icon: './travel.png' },
-  { name: 'Meditate', icon: './meditate.png' },
-  { name: 'Play', icon: './play.png' },
+  { name: 'Exercise', icon: './assets/pic1.png' },
+  { name: 'Study', icon: './assets/exercise.png' },
+  { name: 'Code', icon: './assets/code.png' },
+  { name: 'Cook', icon: './assets/cook.png' },
+  { name: 'Read', icon: './assets/read.png' },
+  { name: 'Travel', icon: './assets/travel.png' },
+  { name: 'Meditate', icon: './assets/meditate.png' },
+  { name: 'Play', icon: './assets/play.png' },
 ];
 
 const initialTasks = [
@@ -38,12 +38,23 @@ const App = () => {
       <View style={styles.first}>
      <View style={styles.container1}>
      <Text style={styles.title}>Hello, Spencer</Text>
-      <Text>10 tasks today</Text>
+      <Text>14 tasks today</Text>
      </View>
       <View style={styles.profilePic}>
         <View style={styles.eclipse}>
-
+            <Image source={require('./assets/person.png')} style={styles.person} />
         </View>
+      </View>
+     </View>
+     <View style={styles.search}>
+      <View style={styles.searchbox}>
+        <View style={styles.search1}>
+          <Image source={require('./assets/Vector1.png')} style={styles.searchpic} />
+          <Text style={styles.searchtext}>Search </Text>
+        </View>
+      </View>
+      <View style={styles.filter}>
+        <Image source={require('./assets/Vector.png')} style={styles.filterpic}/>
       </View>
      </View>
       <Text style={styles.subtitle}>Categories</Text>
@@ -76,9 +87,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#f7f0e8',
   },
   eclipse: {
-    backgroundColor: 'blue', 
+    backgroundColor: '', 
     width: 50,
     height: 52,
+    borderRadius: 25,
+  },
+  person: {
+    width: 46,
+    height: 45,
+    backgroundColor: 'white',
     borderRadius: 25,
   },
   first: {
@@ -87,11 +104,51 @@ const styles = StyleSheet.create({
     backgroundColor: '',
   },
   profilePic: {
-    backgroundColor: 'red',
+    backgroundColor: '',
     width: 50,
     height: 52,
   },
-  
+  search: {
+    backgroundColor: '#f7f0e8',
+    height: 49,
+    width: 353,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  searchbox: {
+    backgroundColor: 'white',
+    height: 48,
+    width: 280,
+    borderRadius: 15,
+  },
+  search1: {
+    height: 24,
+    width: 82,
+    marginTop: 12,
+    marginLeft: 10,
+    flexDirection: 'row',
+  },
+  searchpic: {
+    marginTop: 2.5,
+  },
+  filter: {
+    backgroundColor: '#F0522F',
+    width: 50,
+    height: 48,
+    borderRadius: 14,
+  },
+  filterpic: {
+    width: 28.33,
+    height: 25.5,
+    marginTop: 10,
+    marginLeft: 8.5,
+  },
+  searchtext: {
+    fontSize: 16,
+    marginLeft: 5,
+    fontWeight: '700',
+  },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
